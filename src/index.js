b35e48c56f88345e200ec3dc622bb8c204f15032
Clifton <117292357+Clifton-TS@users.noreply.github.com>
2024-03-06 10:34:35 -0300

    Change bg and add home skeleton

--- src/index.js
+++ src/index.js
@@ -1,10 +1,9 @@
-import './style.css'
-import './logo.png'
+import "./reset.css";
+import "./style.css";
+import "./images/logo.png";
 
 // let a = document.createElement("div")
 // a.classList.add("test")
 // a.textContent = "test"
 
-
-
-document.body.appendChild(a)
+document.body.appendChild(a);
